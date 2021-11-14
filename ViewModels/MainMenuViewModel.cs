3300598cb694e610d616ef24efa08f7bd6a1adf2
autumnman90 <matthaeus.herbst@gmx.de>
2021-11-14 13:19:19 +0100

    Initial commit

---
+++ ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Runner.Views;
+using Runner.Models;
+
+namespace Runner.ViewModels
+{
+    class MainMenuViewModel : MainWindow
+    {
+        public DelegateCommandModel StartGame { get; set; }
+        
+
+
+        public MainMenuViewModel()
+        {
+            this.StartGame = new DelegateCommandModel(
+                (o) => { mainFrame.NavigationService.Navigate(new GameViewModel()); }
+                );
+        }
+
+    }
+}
